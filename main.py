6a4f9e457bbd7fffa3c98ffeb829b7d8cc0fa89a
Vanadse <legend_of_mana1@yahoo.com.ar>
2025-05-27 09:39:57 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+
+
+
+
+# Dont edit below here-------------------
+
+if __name__ == "__main__":
+    main()
